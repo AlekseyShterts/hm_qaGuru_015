2c52fcb615a22c06621e642a6851879c892bb40e
AShterts <a.shterts.v@yandex.ru>
2024-08-31 10:21:13 +0300

    Change log type on log().all()

--- src/test/java/specs/RequestSpec.java
+++ src/test/java/specs/RequestSpec.java
@@ -5,13 +5,12 @@ import io.restassured.specification.RequestSpecification;
 import static helpers.CustomAllureListener.withCustomTemplates;
 import static io.restassured.RestAssured.with;
 import static io.restassured.http.ContentType.JSON;
+import static java.util.logging.Level.ALL;
 
 public class RequestSpec {
     public static RequestSpecification defaultRequestSpec = with()
             .filter(withCustomTemplates())
-            .log().uri()
-            .log().body()
-            .log().headers()
+            .log().all()
             .contentType(JSON);
 
 }
